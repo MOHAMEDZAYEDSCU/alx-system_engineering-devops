1ebfee411ae540488f72f9c230fb15269bc11b26
mohamed zayed <mohamedzayedgaber111.com>
2023-08-17 13:18:44 +0300

    commit no 1

--- commit_adder.sh
+++ commit_adder.sh
@@ -1,26 +1,17 @@
 #!/bin/bash
 
-# Change to your Git repository directory
-cd /path/to/your/git/repository
-
-# Loop to add, commit, and push 100 times
-for ((i=1; i<=100; i++))
+for ((i=1; i<=5; i++))
 do
     echo "Performing iteration $i"
-    # Make changes to your repository here (e.g., modifying files)
     
-    # Add changes
+    touch newfile_$i.txt
+    
     git add .
-    
-    # Commit changes
-    git commit -m "Commit $i"
-    
-    # Push changes
-    git push origin master
-    
-    # Optional: Sleep for a few seconds between iterations
+    git commit -m "commit no $i"
+    git push
+    rm newfile_$i.txt
+
     sleep 5
 done
 
-echo "Finished 100 iterations"
-
+echo "Finished 5 iterations"
